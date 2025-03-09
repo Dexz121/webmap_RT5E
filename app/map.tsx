3b1341dbe39ac12c79bfbfef7ec028af001f01c3
EnriqueGV97 <gave.enrique94@gmail.com>
2025-03-09 13:47:32 -0600

    ESTA SI - instalacion de los estilos con tailwind

--- app/map.tsx
+++ app/map.tsx
@@ -5,7 +5,7 @@ export default function Map() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Stack.Screen options={{ title: 'Mapa Personalizado' }} />
-      <Text>Contenido de la pantalla del mapa</Text>
+      <Text className=' text-red-700'>Contenido de la pantalla del mapa</Text>
     </View>
   );
 }
